36d328f45e44a46c2f1891f914845e41c5b1e806
Waihon Yew <yewwaihon@gmail.com>
2023-04-20 21:21:23 +0800

    Create find_all_substrings.py

---
+++ strings/find_all_substrings.py
@@ -0,0 +1,12 @@
+the_text = """A variation of the ordinary lorem ipsum
+text has been used in typesetting since the 1960s 
+or earlier, when it was popularized by advertisements 
+for Letraset transfer sheets. It was introduced to 
+the Information Age in the mid-1980s by the Aldus Corporation, 
+which employed it in graphics and word-processing templates
+for its desktop publishing program PageMaker (from Wikipedia)"""
+
+idx = the_text.find('the')
+while idx != -1:
+    print(idx)
+    idx = the_text.find('the', idx + 1)
