ed8d614fd2b70caa0365027d3754cb71eb668f90
Waihon Yew <yewwaihon@gmail.com>
2023-05-02 09:35:33 +0800

    Create objective_stack.py

---
+++ oop/objective_stack.py
@@ -0,0 +1,6 @@
+class Stack:  # Defining the Stack class.
+    def __init__(self):  # Defining the constructor function.
+        print("Hi!")
+
+if __name__ == "__main__":
+    stack_object = Stack()  # Instantiating the object.
