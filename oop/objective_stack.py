301d566a77cf940d74205fa5bab44f327c7d5f09
Waihon Yew <yewwaihon@gmail.com>
2023-05-02 09:49:23 +0800

    Add a list for a stack

--- oop/objective_stack.py
+++ oop/objective_stack.py
@@ -1,6 +1,7 @@
-class Stack:  # Defining the Stack class.
-    def __init__(self):  # Defining the constructor function.
-        print("Hi!")
+class Stack:
+    def __init__(self):
+        self.stack_list = []
 
 if __name__ == "__main__":
-    stack_object = Stack()  # Instantiating the object.
+    stack_object = Stack()
+    print(len(stack_object.stack_list))
