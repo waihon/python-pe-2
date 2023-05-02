663d92c96c0101cf378444f989a40ca1a58d5753
Waihon Yew <yewwaihon@gmail.com>
2023-05-02 13:02:25 +0800

    Private property

--- oop/objective_stack.py
+++ oop/objective_stack.py
@@ -1,7 +1,8 @@
 class Stack:
     def __init__(self):
-        self.stack_list = []
+        self.__stack_list = []
 
 if __name__ == "__main__":
     stack_object = Stack()
-    print(len(stack_object.stack_list))
+    # AttributeError: 'Stack' object has no attribute '__stack_list'
+    print(len(stack_object.__stack_list))
