726488cd1009982f2956e556c6bedc95915a699f
Waihon Yew <yewwaihon@gmail.com>
2023-05-02 13:03:22 +0800

    Update objective_stack.py

--- oop/objective_stack.py
+++ oop/objective_stack.py
@@ -2,7 +2,21 @@ class Stack:
     def __init__(self):
         self.__stack_list = []
 
+    def push(self, val):
+        self.__stack_list.append(val)
+
+    def pop(self):
+        val = self.__stack_list[-1]
+        del self.__stack_list[-1]
+        return val
+
 if __name__ == "__main__":
     stack_object = Stack()
-    # AttributeError: 'Stack' object has no attribute '__stack_list'
-    print(len(stack_object.__stack_list))
+    
+    stack_object.push(1)
+    stack_object.push(2)
+    stack_object.push(3)
+    
+    print(stack_object.pop()) # 3
+    print(stack_object.pop()) # 2
+    print(stack_object.pop()) # 1
