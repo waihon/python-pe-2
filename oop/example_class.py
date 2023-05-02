d98f2248257db954a340061092fd3eaf082d334f
Waihon Yew <yewwaihon@gmail.com>
2023-05-02 23:28:37 +0800

    Check existence of property using hasattr

--- oop/example_class.py
+++ oop/example_class.py
@@ -6,17 +6,21 @@ class ExampleClass:
         self.__first = val
         ExampleClass.varia = val
         ExampleClass.__counter += 1
+        if val % 2 != 0:
+            self.a = val
+        else:
+            self.b = val
 
     def set_second(self, val):
         self.__second = val
 
 if __name__ == "__main__":
-    print(ExampleClass.__dict__) # {..., varia: 1, ...``}
+    print(ExampleClass.__dict__) # {..., '_ExampleClass__counter': 0, 'varia': 1, ...``}
     example_object_1 = ExampleClass()
 
     example_object_2 = ExampleClass(2)
     example_object_2.set_second(3)
-    print(ExampleClass.__dict__) # {..., varia: 2, ...``}
+    print(ExampleClass.__dict__) # {..., '_ExampleClass__counter': 2, 'varia': 2, ...``}
 
     example_object_3 = ExampleClass(4)
     example_object_3.__third = 5
@@ -27,3 +31,9 @@ if __name__ == "__main__":
     # {'_ExampleClass__first': 2, '_ExampleClass__second': 3} 3
     print(example_object_3.__dict__, example_object_3._ExampleClass__counter)
     # {'_ExampleClass__first': 4, '__third': 5} 3
+
+    example_object = ExampleClass(1)
+    if hasattr(example_object, 'a'):
+        print('a:', example_object.a) # a: 1
+    if hasattr(example_object, 'b'):
+        print('b:', example_object.b)
