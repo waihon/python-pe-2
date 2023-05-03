70c66f94a511893c4e2e3b902393b662f5526752
Waihon Yew <yewwaihon@gmail.com>
2023-05-03 09:16:33 +0800

    Add constructor

--- oop/classy.py
+++ oop/classy.py
@@ -1,19 +1,23 @@
 class Classy:
     varia = 2
+    def __init__(self, value):
+        self.var = value
+    
     def method(self, parm):
         print("method: ", parm)
-        self.other() # other
 
     def print_vars(self):
-        print(self.varia, self.var)        
+        print(self.varia, self.var)
+        self.other() # other        
 
     def other(self):
         print("other")
 
 if __name__ == "__main__":
-    obj = Classy()
+    obj = Classy("object")
+    print(obj.var)   # object    
     obj.var = 3
     obj.method(1)    # method: 1
     obj.method(2)    # method: 2
     obj.method(3)    # method: 3
-    obj.print_vars() # 2 3
+    obj.print_vars() # 2 3\nother
