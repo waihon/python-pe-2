61fa32887db98316159c413d67898ce8c0f9eb07
Waihon Yew <yewwaihon@gmail.com>
2023-05-03 08:59:55 +0800

    Update classy.py

--- oop/classy.py
+++ oop/classy.py
@@ -1,7 +1,9 @@
 class Classy:
-    def method(self):
-        print("method")
+    def method(self, parm):
+        print("method: ", parm)
 
 if __name__ == "__main__":
-  obj = Classy()
-  obj.method() # method
+    obj = Classy()
+    obj.method(1) # method: 1
+    obj.method(2) # method: 2
+    obj.method(3) # method: 3
