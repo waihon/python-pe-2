ba40f522aeb9dd83d1e61fb22023c184dc95cdcf
Waihon Yew <yewwaihon@gmail.com>
2023-05-03 09:05:58 +0800

    Access class and instance variables using self

--- oop/classy.py
+++ oop/classy.py
@@ -1,9 +1,15 @@
 class Classy:
+    varia = 2
     def method(self, parm):
         print("method: ", parm)
 
+    def print_vars(self):
+        print(self.varia, self.var)        
+
 if __name__ == "__main__":
     obj = Classy()
-    obj.method(1) # method: 1
-    obj.method(2) # method: 2
-    obj.method(3) # method: 3
+    obj.var = 3
+    obj.method(1)    # method: 1
+    obj.method(2)    # method: 2
+    obj.method(3)    # method: 3
+    obj.print_vars() # 2 3
