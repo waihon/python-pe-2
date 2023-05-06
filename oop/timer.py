ea7977f7104f2cc0b20e1f00608287fe62b2be51
Waihon Yew <yewwaihon@gmail.com>
2023-05-06 09:16:28 +0800

    Implement prev_second

--- oop/timer.py
+++ oop/timer.py
@@ -9,20 +9,34 @@ class Timer:
 
     def next_second(self):
         seconds = (self.__seconds + 1) % 60
-        add_minutes = (self.__seconds + 1) // 60
-        minutes = (self.__minutes + add_minutes) % 60
-        add_hours = (self.__minutes + add_minutes) // 60
-        self.__hours = (self.__hours + add_hours) % 24
+        minutes_adj = (self.__seconds + 1) // 60
+        minutes = (self.__minutes + minutes_adj) % 60
+        hours_adj = (self.__minutes + minutes_adj) // 60
+        self.__hours = (self.__hours + hours_adj) % 24
         self.__minutes = minutes
         self.__seconds = seconds
 
     def prev_second(self):
-        pass
-
+        seconds = 59 if self.__seconds == 0 else self.__seconds - 1 
+        minutes_adj = 1 if self.__seconds == 0 else 0
+        self.__seconds = seconds
+        if minutes_adj == 0:
+            return
+        
+        minutes = 59 if self.__minutes == 0 else self.__minutes - 1
+        hours_adj = 1 if self.__minutes == 0 else 0
+        self.__minutes = minutes        
+        if hours_adj == 0:
+            return
+        
+        self.__hours = 23 if self.__hours == 0 else self.__hours - 1
+        
 if __name__ == "__main__":
     timer = Timer(23, 59, 59)
     print(timer) # 23:59:59
     timer.next_second()
     print(timer) # 00:00:00
-    # timer.prev_second()
-    # print(timer) # 23:59:59
+    timer.prev_second()
+    print(timer) # 23:59:59
+    timer.prev_second()
+    print(timer) # 23:59:58
