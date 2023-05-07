b26ed9dc693da935035151626bd4c7a4a8166a68
Waihon Yew <yewwaihon@gmail.com>
2023-05-07 14:51:08 +0800

    Update vehicle.py

--- oop/vehicle.py
+++ oop/vehicle.py
@@ -9,14 +9,29 @@ class LandVehicle(Vehicle):
 class TrackedVehicle(LandVehicle):
     pass
 
+
 if __name__ == "__main__":
     for cls1 in [Vehicle, LandVehicle, TrackedVehicle]:
         for cls2 in [Vehicle, LandVehicle, TrackedVehicle]:
             # Each class is considered to be a subclass of itself.
             print(issubclass(cls1, cls2), end="\t")
         print()
-        """
-        True  False   False
-        True  True    False
-        True  True    True
-        """
+    """
+    True  False   False
+    True  True    False
+    True  True    True
+    """
+
+    my_vehicle = Vehicle()
+    my_land_vehicle = LandVehicle()
+    my_tracked_vehicle = TrackedVehicle()
+    
+    for obj in [my_vehicle, my_land_vehicle, my_tracked_vehicle]:
+        for cls in [Vehicle, LandVehicle, TrackedVehicle]:
+            print(isinstance(obj, cls), end="\t")
+        print()
+    """
+    True    False   False
+    True    True    False
+    True    True    True
+    """
