77126193028daa7f6700a9a6604c0f07d41d28f8
Waihon Yew <yewwaihon@gmail.com>
2023-05-07 19:53:14 +0800

    Level2 overrides Level1

--- oop/level_1_2_3.py
+++ oop/level_1_2_3.py
@@ -1,14 +1,18 @@
 class Level1:
     variable_1 = 100
+    var = 100
     def __init__(self):
         self.var_1 = 101
 
     def fun_1(self):
         return 102
 
+    def fun(self):
+        return 101
 
 class Level2(Level1):
     variable_2 = 200
+    var = 200
     def __init__(self):
         super().__init__()
         self.var_2 = 201
@@ -16,6 +20,8 @@ class Level2(Level1):
     def fun_2(self):
         return 202
 
+    def fun(self):
+        return 201
 
 class Level3(Level2):
     variable_3 = 300
@@ -32,3 +38,5 @@ if __name__ == "__main__":
     print(obj.variable_1, obj.var_1, obj.fun_1()) # 100 101 102
     print(obj.variable_2, obj.var_2, obj.fun_2()) # 200 201 202
     print(obj.variable_3, obj.var_3, obj.fun_3()) # 300 301 302
+    print(obj.var, obj.fun())                     # 200 201
+
