818559beccb04ab1788f64f58b389c1703473c7a
Waihon Yew <yewwaihon@gmail.com>
2023-05-07 14:37:37 +0800

    Use issubclass to check cls1 is a subclass of cls2

--- oop/vehicle.py
+++ oop/vehicle.py
@@ -6,5 +6,17 @@ class LandVehicle(Vehicle):
     pass
 
 
-class TrackedVehiche(LandVehicle):
+class TrackedVehicle(LandVehicle):
     pass
+
+if __name__ == "__main__":
+    for cls1 in [Vehicle, LandVehicle, TrackedVehicle]:
+        for cls2 in [Vehicle, LandVehicle, TrackedVehicle]:
+            # Each class is considered to be a subclass of itself.
+            print(issubclass(cls1, cls2), end="\t")
+        print()
+        """
+        True  False   False
+        True  True    False
+        True  True    True
+        """
