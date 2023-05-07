db0420a65bc11e20f14431f853a9920ac7324727
Waihon Yew <yewwaihon@gmail.com>
2023-05-07 16:02:28 +0800

    Update super_sub.py

--- oop/super_sub.py
+++ oop/super_sub.py
@@ -1,4 +1,6 @@
 class Super:
+    sup_var = 1
+    
     def __init__(self, name):
         self.name = name
 
@@ -7,6 +9,8 @@ class Super:
 
 
 class Sub(Super):
+    sub_var = 2
+    
     def __init__(self, name):
         # The super() function creates a context in which
         # you don't have to (moreover, you mustn't) pass
@@ -17,4 +21,6 @@ class Sub(Super):
 if __name__ == "__main__":
     obj = Sub("Andy")
     
-    print(obj) # My name is Andy.
+    print(obj)         # My name is Andy.
+    print(obj.sub_var) # 2
+    print(obj.sup_var) # 1
