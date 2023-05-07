49c2a5106d5b7d2b8f48b09f48942422ea084ee3
Waihon Yew <yewwaihon@gmail.com>
2023-05-07 15:57:08 +0800

    Update super_sub.py

--- oop/super_sub.py
+++ oop/super_sub.py
@@ -8,7 +8,10 @@ class Super:
 
 class Sub(Super):
     def __init__(self, name):
-        Super.__init__(self, name)
+        # The super() function creates a context in which
+        # you don't have to (moreover, you mustn't) pass
+        # the self argument to the method being invoked.
+        super().__init__(name)
 
 
 if __name__ == "__main__":
